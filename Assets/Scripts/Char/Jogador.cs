9db845ff1c62cafc3d639c18146e5c4e9e48895f
EmliOMago <emiliodesouzajunior@gmail.com>
2025-08-25 16:33:38 -0300

    :tada: :sparkles: Primeiro Comit
    :tada: :sparkles: feat: primeiro comit do jogo da atividade do Senac

---
+++ Assets/Scripts/Char/Jogador.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    public class Jogador : MonoBehaviour
+    {
+        [HideInInspector] public int moedas;
+        public float jVelocidade = 10;
+        public float jVelocidadeMaxima = 10;
+        public float forcaPulo = 150;
+        public float jVelocidadeMaximaPulo = 10;
+
+        public Transform groundChecker;
+
+        [HideInInspector] public float jHorizontal;
+        [HideInInspector] public float movimento;
+        [HideInInspector] public bool pulo = false;
+
+        [HideInInspector] public bool podePular = true;
+        [HideInInspector] public bool morto = false;
+        Rigidbody2D rigidB;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            moedas = 0;
+            rigidB = transform.GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            movimento = Input.GetAxisRaw("Horizontal") * jVelocidade;
+            pulo = Input.GetAxis("Jump") != 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            morto = Physics2D.OverlapBox
+                (
+                groundChecker.position,
+                groundChecker.GetComponent<BoxCollider2D>().bounds.size,
+                0,
+                LayerMask.GetMask("Morte")
+                );
+
+            Debug.Log(morto);
+            Debug.Log(podePular);
+
+            // Logica da movimentação
+            float movimeto = Input.GetAxisRaw("Horizontal") * Time.deltaTime * jVelocidade;
+            transform.position += new Vector3(movimeto, 0);
+
+            // Logica do pulo
+            bool pulo = Input.GetButtonDown("Jump");
+
+            if (pulo && podePular)
+            {
+                rigidB.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
+                podePular = false;
+            }
+
+            // Forma 2 de pulo: verificando sobreposição de colissões
+            podePular = Physics2D.OverlapBox
+                (
+                groundChecker.position,
+                groundChecker.GetComponent<BoxCollider2D>().bounds.size,
+                0,
+                LayerMask.GetMask("Ground")
+                );
+
+            // verificação de morte
+            if (morto)
+            {
+                transform.position = new Vector2(0, 0);
+            }
+
+
+        }
+    }
+}
